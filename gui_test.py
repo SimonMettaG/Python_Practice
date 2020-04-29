5c9da11dc519703396040e435e425d22b774a606
simon <simon.mettag@gmail.com>
2020-04-29 14:29:24 -0500

    First upload!

---
+++ gui_test.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+ventana_principal = Tk()
+ventana_principal.title('Base de Datos con Neo4j')
+ventana_principal.geometry('500x500')
+
+icono = PhotoImage(file = './img/burger.png')
+ventana_principal.iconphoto(False, icono)
+
+boton_salir = Button(ventana_principal, text='Salir', width=10, height=2, command=ventana_principal.destroy)
+boton_salir.place(relx=0.5, rely=0.9, anchor = CENTER)
+
+ventana_principal.mainloop()
+
+"""
+r = tk.Tk() 
+r.title('Counting Seconds')
+r.geometry("500x500")
+
+icon = tk.PhotoImage(file = "./img/burger.png")
+r.iconphoto(False, icon)
+r.positionfrom()
+button = tk.Button(r, text='Stop', width=25, command=r.destroy) 
+button.pack() 
+r.mainloop()
+"""
